394e274d130fdaffe3a37a5786cb74a0e8daa697
Matheus Vidal <102569695+matheusvidal21@users.noreply.github.com>
2023-07-16 20:01:06 -0300

    Update main.cpp

--- Manipulação de arquivos/Q4/main.cpp
+++ Manipulação de arquivos/Q4/main.cpp
@@ -1,22 +1,10 @@
-/**
- * @file main.cpp
- * @brief Questão 4) Você deve escrever um programa em C++ usando Orientação a Objetos que, dado o arquivo com a relação de funcionários, imprima o valor gasto com a folha de pagamento para um determinado departamento. Os dados dos funcionários da empresa devem ser fornecidos para um vetor, de modo que uma função que execute tal tarefa deve ser implementada
- * @author Matheus Vidal (20220055246)
- * @since 25/05/2022
- * @date 02/06/2022 
- */
 #include <iostream>
 #include <fstream>
 #include "./modularizacao/classes.h"
 #include "./modularizacao/functions.h"
 using namespace std;
 
-/**
- * @brief Função principal do programa.
- * @return Retorna 0 em caso de sucesso.
- * 
- * @details A função main é o ponto de entrada do programa e executa uma série de operações. Ela abre um arquivo de entrada e um arquivo de saída, verifica se os arquivos foram abertos corretamente, lê a quantidade de funcionários do arquivo de entrada e cria um vetor dinâmico de objetos Funcionario. Em seguida, chama as funções copia_dados, imprime_folha_pagamento e cria_saida para manipular os dados dos funcionários e gerar uma folha de pagamento. Por fim, fecha os arquivos, libera a memória alocada e encerra o programa.
- */
+
 int main() {
   ifstream arquivoEntrada("./arquivos/funcionarios.txt");
   ofstream arquivoSaida("./arquivos/folha_de_pagamento.txt");
